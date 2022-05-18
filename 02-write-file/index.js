beb25d2a7a170256d5959dbec6f6cf48cf33c2e1
diana2886 <tdm2886@gmail.com>
2022-05-18 11:50:13 +0600

    feat: add 04-copy-directory/index.js and change 02-write-file/index.js

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-const process = require('process');
-const input = process.stdin;
-const output = process.stdout;
+const { stdin: input, stdout: output } = require('process');
 
 const stream = fs.createWriteStream(path.join(__dirname,'text.txt'));
 const rl = readline.createInterface({ input, output });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 
+async function copyDir(src, copy) {
+  await fs.promises.mkdir(copy, { recursive: true });
+  let entries = await fs.promises.readdir(src, { withFileTypes: true });
+  for (let entry of entries) {
+    let srcPath = path.join(src, entry.name);
+    let copyPath = path.join(copy, entry.name);
+    if (entry.isDirectory()) {
+      await copyDir(srcPath, copyPath);
+    } else {
+      await fs.promises.copyFile(srcPath, copyPath);
+    }
+  }
+}
+
+function copyUpdatedDir(src, copy) {
+  fs.rm(copy, { recursive: true, force: true }, () => copyDir(src, copy));
+}
+
+copyUpdatedDir(path.join(__dirname, '/files'), path.join(__dirname, '/files-copy'));
